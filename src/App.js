cb2e3b6be88eec8a6f653db01f875b7b410ef0df
jhonmeche <jhonmeche96@gmail.com>
2022-01-14 01:51:54 -0500

    Creacion de componente Form
    se creo el imput y el boton para ingresar y enviar la tarea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Todoform from './components/Todoform';
 
 function App() {
   return (
     <div className="todo-app">
-  <h1> To-do App </h1>
+      <Todoform/>
     </div>
   );
 }
